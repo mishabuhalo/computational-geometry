ca4c0a00ca24ed8dc4014a836e40f6504e0d5f41
Misha Bukhalo <bukhalo1999@gmai.com>
2020-05-07 20:54:22 +0300

    Added realization of hermite interpolation

---
+++ Hermite interpolation/Hermite interpolation/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Hermite_interpolation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(1, 2);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(799, 444);
+            this.zedGraphControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+    }
+}
+
---
+++ Hermite interpolation/Hermite interpolation/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Hermite_interpolation
+{
+    public partial class Form1 : Form
+    {
+        const int n = 9;
+        double[] x = new double[n] {1,2,3,4,5,6,7,9,10};
+        double[] y = new double[n] {4,3,2,4,1,4,3,1,2};
+        double[] derivative = new double[n];
+
+        List<double> t = new List<double>();
+        List<double> result = new List<double>();
+
+        public void computeDerivative()
+        {
+            derivative[0] = derivative[n - 1] = 0;
+            for (int i =1; i < n-1; i++)
+            {
+                derivative[i] = 0.5 * (((y[i+1]-y[i])/(x[i+1]-x[i]))+((y[i]-y[i-1])/(x[i]-x[i-1])));
+            }
+        }
+
+        public void computeHermitfunction()
+        {
+            for(int i = 0; i < n-1; i ++)
+            {
+                for(double j = x[i]; j<x[i+1]; j+=0.2)
+                {
+                    double temp = (j - x[i]) / (x[i + 1] - x[i]);
+                    
+                    t.Add(temp);
+
+                    double res = (2 * Math.Pow(temp, 3) - 3 * temp * temp + 1) * y[i] + (Math.Pow(temp, 3) - 2 * temp * temp+ temp) * (x[i + 1] - x[i]) * derivative[i]
+                        + (-2 * Math.Pow(temp, 3) + 3 * temp * temp) * y[i + 1] + (Math.Pow(temp, 3) - temp * temp) * (x[i + 1] - x[i]) * derivative[i + 1];
+
+                    result.Add(res);
+                }
+            }
+
+            // Видаляю елементи які не потрібні, вони додались, через те що тип double неточний
+            result.RemoveAt(5);
+            result.RemoveAt(40);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            computeDerivative();
+
+            computeHermitfunction();
+
+            DrawGraph();
+        }
+
+        public void DrawGraph()
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+
+            pane.CurveList.Clear();
+
+            PointPairList list = new PointPairList();
+            int j = 0;
+            for(double i = x[0]; i <=x[n-1]; i+=0.2)
+            {
+                list.Add(i, result[j]);
+                j++;
+            }
+
+            LineItem myCurve = pane.AddCurve("Hermite", list, Color.Blue, SymbolType.None);
+
+            PointPairList linear = new PointPairList();
+
+            for(int i = 0; i <n; i ++)
+            {
+                linear.Add(x[i], y[i]);
+            }
+
+            LineItem linearCurve = pane.AddCurve("Linear", linear, Color.Red, SymbolType.None);
+
+            pane.AddCurve("", linear, Color.Red, SymbolType.Circle);
+
+
+            zedGraphControl1.AxisChange();
+
+            zedGraphControl1.Invalidate();
+        }
+
+    }
+}
