71991a7fd27b8193dd75f0dcb23a2a365e23f6b4
Misha Bukhalo <bukhalo1999@gmai.com>
2020-05-09 14:21:06 +0300

    Added realization of Bezier surface generation

--- Bezier surface/Bezier surface/Form1.Designer.cs
+++ Bezier surface/Bezier surface/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nChartControl1 = new Nevron.Chart.WinForm.NChartControl();
+            this.SuspendLayout();
+            // 
+            // nChartControl1
+            // 
+            this.nChartControl1.AutoRefresh = false;
+            this.nChartControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.nChartControl1.InputKeys = new System.Windows.Forms.Keys[0];
+            this.nChartControl1.Location = new System.Drawing.Point(120, 21);
+            this.nChartControl1.Name = "nChartControl1";
+            this.nChartControl1.Size = new System.Drawing.Size(533, 379);
+            this.nChartControl1.State = ((Nevron.Chart.WinForm.NState)(resources.GetObject("nChartControl1.State")));
+            this.nChartControl1.TabIndex = 0;
+            this.nChartControl1.Text = "nChartControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nChartControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Nevron.Chart.WinForm.NChartControl nChartControl1;
     }
 }
 
--- Bezier surface/Bezier surface/Form1.cs
+++ Bezier surface/Bezier surface/Form1.cs
@@ -7,14 +7,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Nevron.GraphicsCore;
+using Nevron.Chart;
+using Nevron.Chart.Windows;
+using Nevron.Chart.WinForm;
+using Nevron.Editors;
 
 namespace Bezier_surface
 {
     public partial class Form1 : Form
     {
+        List<double> x = new List<double>() { 1, 2, 3, 4, 5, 6, 7, 9 };
+        List<double> y = new List<double>() { 4, 3, 2, 4, 1, 4, 3, 1 };
+        List<double> z = new List<double>() { 1, 0, 2, 1, 2, 0, 3, 3 };
+
+        List<double> resultX = new List<double>();
+        List<double> resultY = new List<double>();
+        List<double> resultZ = new List<double>();
+        const int n = 2;
+        const int m = 4;
+
+        public double factorial(int number)
+        {
+            double result = 1;
+            while (number > 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+
+        public double Bernstein(int first, int second, double third)
+        {
+            return (factorial(first) / (factorial(second) * factorial(first - second))) * Math.Pow(third, second) * Math.Pow(1 - third, first - second);
+        }
+
+        public List<double> Bezier(double value)
+        {
+            List<double> result = new List<double>();
+            for (double u = 0.1; u < 1; u += 0.05)
+            {
+                for (double v = 0.1; v < 1; v += 0.05)
+                {
+                    double temp = 0;
+                    for (int i = 0; i <= n; i++)
+                    {
+                        for (int j = 0; j <= m; j++)
+                        {
+                            temp += Bernstein(n, i, u) * Bernstein(m, j, v) * value;
+                        }
+                    }
+                    result.Add(temp);
+                }
+            }
+            return result;
+        }
+
+        public void computation()
+        {
+            for (int i = 0; i < 8; i++)
+                resultX.AddRange(Bezier(x[i]));
+
+            for (int i = 0; i < 8; i++)
+                resultY.AddRange(Bezier(y[i]));
+
+            for (int i = 0; i < 8; i++)
+                resultZ.AddRange(Bezier(z[i]));
+        }
+
+        public void generateSurface()
+        {
+            NCartesianChart chart;
+
+            //nChartControl1.Clear();
+            NTriangulatedSurfaceSeries surface;
+
+            NZoomTool zt = new NZoomTool();
+            zt.BeginDragMouseCommand = new NMouseCommand(MouseAction.Wheel, MouseButton.Middle, 0);
+            zt.ZoomStep = 20;
+
+
+            nChartControl1.Controller.Tools.Add(zt);
+
+            nChartControl1.Controller.Tools.Add(new NSelectorTool());
+            nChartControl1.Controller.Tools.Add(new NTrackballTool());
+
+            nChartControl1.Settings.JitterMode = JitterMode.Enabled;
+            nChartControl1.Settings.JitteringSteps = 1;
+
+            chart = (NCartesianChart)nChartControl1.Charts[0];
+
+            chart.Enable3D = true;
+            chart.Width = chart.Height = chart.Depth = 50;
+            chart.Projection.SetPredefinedProjection(PredefinedProjection.PerspectiveTilted);
+
+            surface = new NTriangulatedSurfaceSeries();
+
+            chart.Series.Add(surface);
+
+            surface.FrameMode = SurfaceFrameMode.Mesh;
+
+            surface.FillMode = SurfaceFillMode.Zone;
+
+            for(int i = 0; i < resultX.Count(); i ++)
+            {
+                surface.XValues.Add(resultX[i]);
+                surface.ZValues.Add(resultZ[i]);
+                surface.Values.Add(resultY[i]);
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+
+            computation();
+            generateSurface();
         }
     }
 }
